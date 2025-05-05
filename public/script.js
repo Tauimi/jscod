3cfe2fd52371fe1439a127bdc5b03f37f4d5d7a4
Tauimi <qqdslk@gmail.com>
2025-05-05 21:26:53 +0300

    ек

--- public/script.js
+++ public/script.js
@@ -6,3 +6,24 @@ document.getElementById('login-link').addEventListener('click', function() {
 document.getElementById('close-modal').addEventListener('click', function() {
     document.getElementById('auth-modal').style.display = 'none';
 });
+// Получаем элемент для переключения темы
+const themeToggle = document.getElementById('theme-toggle');
+
+// Проверяем, сохранена ли тема в localStorage
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark');
+    themeToggle.classList.add('dark');
+}
+
+// Переключаем тему
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    themeToggle.classList.toggle('dark');
+
+    // Сохраняем выбранную тему
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
